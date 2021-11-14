47f9ef6e5e402b56d8cee599d038f2dc8000986c
zqf <1936534366@qq.com>
2021-11-14 13:29:49 +0800

    1

--- src/main/java/com/example/springbootrabbitmq/config/rabbitmqconfig.java
+++ src/main/java/com/example/springbootrabbitmq/config/rabbitmqconfig.java
@@ -1,8 +1,43 @@
 package com.example.springbootrabbitmq.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class rabbitmqconfig {
-    
+    //交换机
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return new FanoutExchange("amq.fanout",true,false);
+    }
+    //设置消息队列
+    @Bean
+    public Queue squeue(){
+        return new Queue("szqf",true);
+    }
+    @Bean
+    public Queue dqueue(){
+        return new Queue("dzqf",true);
+    }
+    @Bean
+    public Queue equeue(){
+        return new Queue("ezqf",true);
+    }
+    //完成绑定关系
+    @Bean
+    public Binding sbinding(){
+        return BindingBuilder.bind(squeue()).to(fanoutExchange());
+    }
+    @Bean
+    public Binding dbinding(){
+        return BindingBuilder.bind(dqueue()).to(fanoutExchange());
+    }
+    @Bean
+    public Binding ebinding(){
+        return BindingBuilder.bind(equeue()).to(fanoutExchange());
+    }
 }
--- src/test/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
+++ src/test/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.springbootrabbitmq;
 
+import com.example.springbootrabbitmq.service.orderservice;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootRabbitmqApplicationTests {
-
+    @Autowired
+    private orderservice orderservice;
     @Test
     void contextLoads() {
+        orderservice.makeorder("z","10",20);
     }
 
 }
